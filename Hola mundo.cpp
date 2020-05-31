5fb3639c62d32d9227f2c5446d07c791006c935f
MateoQuintanilla <mjquintanilla@espe.edu.ec>
2020-05-31 13:04:24 -0500

    primer commit

---
+++ Hola mundo.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+
+   cout<<"Hola mundo!!"<<endl;
+    system("pause");
+    return 0;
+
+
+
+}
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
